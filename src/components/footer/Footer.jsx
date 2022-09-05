b197ed7093d194541ee52ef65d0799529cfbb758
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-09-05 21:54:45 +0100

    hey

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,10 +4,10 @@ import {BsSearch } from 'react-icons/bs'
 import {FiTwitter,FiGithub} from 'react-icons/fi'
 function Footer() {
   return (
-    <div className='bg-ft text-white h-[40vh] flex justify-between p-4'>
-        <div>
+    <div className='bg-ft text-white h-max mflex md:justify-between p-4 flex-cols'>
+        <div className='h-max pb-4'>
             <h3 className='text-center mb-4'>Eradz</h3>
-            <div className='flex gap-x-2'>
+            <div className='flex justify-center gap-x-2'>
             <a href='https://www.instagram.com/andrewanagu' className='text-white bg-icon p-1 rounded-[50%]'><AiOutlineInstagram/></a>
             <a href='https://twitter.com/anaguchidi35?t=7w-IG_UxIgwHm4ulrzpDSA&s=08' className='text-white  bg-icon p-1 rounded-[50%]'><FiTwitter/></a>
             <a href='https://github.com/eradz' className='text-white  bg-icon p-1 rounded-[50%]'><FiGithub/></a>
@@ -15,7 +15,7 @@ function Footer() {
         </div>
         <div className='flex justify-between gap-x-3'>
         <div>
-            <h5 >About Us</h5>
+            <h5>About Us</h5>
             <div className='text-text'>
                 <p>Blog</p>
                 <p>Return Policy</p>
@@ -47,8 +47,8 @@ function Footer() {
         </div>
         <div>
             <h5>Search Product</h5>
-            <div className='relative  w-[95%] mx-auto'>
-                <input className='px-9 py-2 relative w-full text-xs border-solid border-2 border-stone-900 rounded-[20px]' type='search' placeholder='Search products, brands and categories'/>
+            <div className='relative '>
+                <input className='px-9 py-2 relative w-full bg-icon text-xs border-solid border-2 border-stone-900 rounded-[20px]' type='search' placeholder='Search'/>
                 <BsSearch className='absolute top-3 left-3 ' />
             </div>
         </div>
