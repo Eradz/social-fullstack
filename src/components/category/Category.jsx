4927ea3b052ccf954668baa6e7a93a302d23d31e
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-09-09 13:05:48 +0100

    cleaning up code

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import watch from '../../assest/watch.png'
 import jewelry from '../../assest/jewelry.png'
 import tv from '../../assest/tvs.png'
@@ -8,21 +8,21 @@ import couch from '../../assest/couch.png'
 import { motion } from "framer-motion"
 function Category() {
     const categories = [
-        {id:1, name: 'Wrist-watches', button: 'View products', img: watch },
-        {id:2, name: 'Furnitures', button: 'View products', img: couch },
-        {id:3, name: 'Jewelry', button: 'View products', img: jewelry  },
-        {id:4, name: 'Electronics', button: 'View products', img: tv },
-        {id:5, name: 'Male wears', button: 'View products', img: men },
-        {id:6, name: 'Female wears', button: 'View products', img: woman },
+        {id:1, name: 'Wrist-watches', button: 'View products', img: watch, bg:'bg-cyan-400' },
+        {id:2, name: 'Furnitures', button: 'View products', img: couch, bg: 'bg-green-300'},
+        {id:3, name: 'Jewelry', button: 'View products', img: jewelry, bg: 'bg-purple-400'},
+        {id:4, name: 'Electronics', button: 'View products', img: tv, bg: 'bg-orange-300'},
+        {id:5, name: 'Male wears', button: 'View products', img: men, bg:'bg-orange-400' },
+        {id:6, name: 'Female wears', button: 'View products', img: woman, bg: 'bg-gray-400'},
     ]
     
-
+   
   return (
     <div className='flex justify-center items-center flex-col mt-5'>
         <h1 className='font-bold mb-4'>All Categories</h1>
         <div className="grid md:grid-cols-3  gap-4">
-           { categories.map((item, i) => {
-           return <div key={item.id} className= "bg-gray-400 shadow shadow-black w-[100%] flex justify-between pl-8 rounded-[50px]">
+           { categories.map((item) => {
+           return <div key={item.id} className= {`${item.bg} shadow shadow-black w-[100%] flex justify-between pl-8 rounded-[50px]`} >
             <div className="my-auto">
             <h5 className='font-semibold pl-2'>{item.name}</h5>
             <button className='mt-2 px-3 py-2.5 rounded-[25px] bg-white' >{item.button}</button>
