47ba30672efb11779cb33868f519362b77cc59c1
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-09-12 16:02:16 +0100

    registeration

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer  from './components/footer/Footer';
 import Home from './components/Home';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Categories from './components/categories/Categories';
+import Register from './components/register/Register';
 function App() {
 
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
       <Route exact path='/' element={<Home/>} />
       <Route exact path='/category' element={<Categories/>} />
+      <Route exact path='/register' element={<Register/>} />
       </Routes>
     </Router>
     <Footer/>
