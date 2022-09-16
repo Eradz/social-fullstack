6824c3bddc039ee386dac6e3e31151ca021027c0
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-09-16 16:33:28 +0100

    added images on product page

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { AiFillHeart } from 'react-icons/ai'
+import { BiArrowFromRight } from 'react-icons/bi'
 import { useLocation } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import itemsSlice from '../redux/items/itemsSlice';
 
-export const Product = (props) => {
-  console.log(props)
+export const Product = () => {
   const location = useLocation()
-  console.log(location)
+  console.log(location.state.from)
+  const item = location.state.from
+
+
+  const [on, setOn] = useState(false)
+  const toggle = ()=>{
+          setOn(!on)
+  }
   return (
-    <div>Product</div>
+    <div>
+      <div className='flex justify-between'>
+        <BiArrowFromRight size={25}/>
+        <span>{item.title}</span>
+        <div onClick={() =>toggle()} className={on? `bg-red-500 p-2 rounded-[50%] cursor-pointer`: `bg-icon p-2 rounded-[50%] cursor-pointer`}>
+          <AiFillHeart color='white'/>
+        </div>
+      </div>
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)} >
+        {item.images.map((images) => {
+         return <SwiperSlide className='w-[200px] h-[300px]'><img src={images} alt={item.title} /></SwiperSlide>
+        })
+    
+        }
+    </Swiper>
+    </div>
   )
 }
