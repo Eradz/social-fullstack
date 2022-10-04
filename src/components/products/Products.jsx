a1edf4685f05bcbbacc7586e5d0ff407901e034e
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-10-04 12:27:04 +0100

    formatted product

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,118 +1,114 @@
-import React, { useState }from 'react'
-import {IoIosArrowDropdown} from 'react-icons/io'
+import React, { useEffect} from 'react'
 import {AiFillStar, AiFillHeart} from 'react-icons/ai'
-import { getItems } from '../redux/items/itemsSlice'
-import { useEffect } from 'react'
-import {  fav, notFave } from '../redux/items/eleSlice'
 import { useDispatch, useSelector } from 'react-redux'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {  Navigation, A11y } from 'swiper';
 import {Link} from 'react-router-dom'
-
+import { getElec, fav, notFave } from '../redux/items/eleSlice'
+import { FaRegEye } from 'react-icons/fa'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {  Scrollbar, A11y } from 'swiper';
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
-import { FaRegEye } from 'react-icons/fa'
+import 'swiper/css/scrollbar';
+import { IoIosArrowDropdown } from 'react-icons/io'
 export const Products = () => {
-  const dispatch = useDispatch()
-  const {items, isload} = useSelector((state) => state.item)
-  const stars = [<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar/>]
-  const stars2 = [<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar className="text-icon"/>]
-  const star5 = stars.map((star) => {
-    return <span className='text-orange-500'>{star}</span>
-})
-  const star4 = stars2.map((star) => {
-    return <span className='text-orange-500'>{star}</span>
-})
-  const [on, setOn] = useState(false)
-  const toggle = (i) => {
-    if(items[i].on){
-     dispatch(notFave(i))
-    }else{
-     dispatch(fav(i))
+    const dispatch = useDispatch()
+    const {elec, isload} = useSelector((state) => state.elec)
+    const star = [<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar/>,<AiFillStar/>]
+    const toggle = (i) => {
+       if(elec[i].on){
+        dispatch(notFave(i))
+       }else{
+        dispatch(fav(i))
+       }
+      
     }
-   
- }
-  localStorage.setItem('items', JSON.stringify(items))
-  useEffect(() => {
-    dispatch(getItems(""))
-  }, [dispatch])
-  if (isload){
-    return(
-      <div>
-        <h1>loading ....</h1>
-        </div>
-    )
-  }
-  
+    
+    useEffect(() => {
+        dispatch(getElec("") )
+    }, [dispatch])
+    if (isload){
+        return(
+        <div>
+            <h1>loading ....</h1>
+            </div>
+        )
+    }
   return (
     <div className='mt-5'>
-      <Link to='/category' className='px-3 flex no-underline items-center justify-between text-black'>
-        <h3 className='' >Top Products</h3>
-        <IoIosArrowDropdown className='-rotate-90' size={25}/></Link>
+      <Link to='/men' className='no-underline text-black font-light'> 
+      <div className='px-3 flex items-center justify-between'>
+        <h3 className='font-bold animate-pulse' >Products</h3>
+        <IoIosArrowDropdown className='-rotate-90' size={25}/>
+      </div>
+      </Link>
       <Swiper className='pb-4'
       // install Swiper modules
       breakpoints={{
         576: {
           width: 576,
           slidesPerView: 2,
-           },
+         
+        },
         768: {
           width: 768,
           slidesPerView: 3,
-           },
+        },
         1024: {
           width: 1024,
           slidesPerView: 4,
-          },
+          navigation: { clickable: true }
+        },
       }}
     
     
-      modules={[ Navigation, A11y,]}
+      modules={[ Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={2}
-      navigation={{ clickable: true }}
+      slidesPerView=  {2}
+      scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
-      >
-      {items.map((item, i) => {
-        if(i < 20){
-          return <SwiperSlide>  
-      <div key={item.id} className='rounded-lg w-max h-[300px] md:h-[320px] shadow shadow-black pt-2 pb-4 px-2 relative bg-white '>
-            <div className=' w-[200px] md:w-[250px] max-h-[300px] h-[70%] mx-auto '> 
-              <div className='flex items-center justify-between bg-white px-2'>
-               <h2>...</h2> 
-               <div onClick={() =>{dispatch(toggle(i))}}  className={item.on? `bg-red-500 p-2 rounded-[50%] cursor-pointer`: `bg-icon p-2 rounded-[50%] cursor-pointer`}>
-                <AiFillHeart color='white'/>
-               </div>
-              </div> 
-              <img src={item.thumbnail} alt={item.title} className=' w-[200px] md:w-full max-h-[300px] h-[80%] mx-auto ' /> 
-            </div>
-            <div className='p-2 h-[50%] flex'>
-            <div >
-              <div className='px-2'>
-                <p>{item.title.length < 20 ? item.title : `${item.title.substring(0, 17)}...`}</p>
+    >
+      {elec.map((item, i) => {
+        if (i < 20 ){
+          return <SwiperSlide key={item.title} >  
+          <div className='rounded-lg w-max md:w-[250px] h-[300px] max-w-max shadow shadow-black pt-2 pb-4 px-2 relative bg-white '>
+                <div className=' w-[180px] md:w-[220px] max-h-[300px] h-[70%] mx-auto '> 
+                  <div className='flex items-center justify-between bg-white px-2'>
+                   <h2>...</h2> 
+                   <div onClick={() =>{dispatch(toggle(i))}}  className={item.on? `bg-red-500 p-2 rounded-[50%] cursor-pointer`: `bg-icon p-2 rounded-[50%] cursor-pointer`}>
+                    <AiFillHeart color='white'/>
+                   </div>
+                  </div> 
+                  <Link to={{pathname: `/product/${item.id}`,}} state={{from: item}} className='text-black no-underline'>
+                      <img src={item.thumbnail} alt={item.title} className=' w-[130px] md:w-[250px] max-h-[300px] h-[80%] mx-auto ' /> 
+                      </Link>
+                </div>
+                <div className='p-2 h-[50%] flex '>
+                <div>
+                  <div className='px-2'>
+                    <p>{item.title.length < 20 ? item.title : `${item.title.substring(0, 17)}...`}</p>
+                  </div>
+                  <h3>${item.price}</h3>
+                  <div className="flex">
+                  {star.map((star) =>{
+                    return <span className='text-orange-500'>{star}</span>
+                  })}
+                  </div>
+                </div>
+                <Link to={{pathname: `/product/${item.id}`,}} state={{from: item}}
+                 className="bg-blue w-max h-max absolute bottom-0 right-0 p-4 rounded-br-lg rounded-tl-lg ">
+                <FaRegEye className='text-white' size={20}/>
+               </Link>
               </div>
-              <h3>${item.price}</h3>
-              <div className='flex'>
-                {item.rating < 4.5 ?
-                  star4 : star5 }
-              </div>
-            </div>
-            <Link to={{pathname: `/product/${item.id}`,}} state={{from: item}}
-             className="bg-blue w-max h-max absolute bottom-0 right-0 p-4 rounded-br-lg rounded-tl-lg ">
-            <FaRegEye className='text-white' size={20}/>
-           </Link>
+    
           </div>
-      </div>
-      </SwiperSlide>
-        }
-        
-      })}
-
-    </Swiper>
-   
-    </div>
-  )
-}
-
+          </SwiperSlide>}
+          })}
+    
+        </Swiper>
+       
+        </div>
+      )
+    }
+    
+    
