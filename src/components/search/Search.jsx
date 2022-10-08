fa0487ef25720c8fd8ab5603f5a2d8f840cccabf
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2022-10-08 01:49:16 +0100

    search good

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 export const Search = ({data}) => {
-  
-    const result = data.map((item) =>{
-        if(item)
-     return (
+    return (
+    <div className='bg-white'>
+    {data.map((item) =>{
+    return (
+        <Link className='hover:bg-blue text-black no-underline' to={{pathname: `/product/${item.id}`,}} state={{from: item}}>
         <div className='hover:bg-blue'>
         <div className='flex px-3 py-2 items-center gap-2'>
-           <img src={item.thumbnail} alt='woman'  className='w-[70px] h-[70px]'/>
-           <div>
-           <h6>{item.title}</h6>
-           <h6>{item.description.slice(0,30)}...</h6>
-           </div>
+        <img src={item.thumbnail} alt='woman'  className='w-[70px] h-[70px]'/>
+        <div>
+        <h6>{item.title}</h6>
+        <h6>{item.description.slice(0,30)}...</h6>
+        </div>
         </div>
     </div>
-     )  
-    })
-    return (
-    
-    <>
-    { result}
-
-    </>
-  )
+    </Link>
+    )  
+    })}
+    </div>
+)
 }
